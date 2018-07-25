d1705ccd3ad994d400beecd373948b8fe7aa6fb5
Jim Byer <jbyer@hotmail.com>
2018-07-25 15:32:34 -0600

    Added some more questions for a total of 10.

--- BrainBuffet/Controllers/QuestionController.cs
+++ BrainBuffet/Controllers/QuestionController.cs
@@ -83,8 +83,16 @@ namespace BrainBuffet.Controllers
                 Id = ++index,
                 QuestionType = "image",
                 ImageUrl = "https://www.coralhills.com/wp-content/uploads/2011/12/St-George-temple.jpg",
-                QuestionText = "Which is the oldest temple still in operation?",
+                QuestionText = "Which temple is this (hint: it is the oldest temple still in operation)?",
                 AnswerText = "St. George, Utah"
+            });
+            q.Add(new Question()
+            {
+                Id = ++index,
+                QuestionType = "image",
+                ImageUrl = "https://mormonhub.com/wp-content/uploads/2014/08/Elder-Bednar.jpg",
+                QuestionText = "What did Elder Bednar talk about in the April 2018 General Conference?",
+                AnswerText = "Being meek and lowly of heart"
             });
             q.Add(new Question()
             {
@@ -97,7 +105,7 @@ namespace BrainBuffet.Controllers
             {
                 Id = ++index,
                 QuestionType = "text",
-                QuestionText = "In what state in the US is the lowest point in the Western Hemisphere (282 feet below sea level)?",
+                QuestionText = "In which US state is the lowest point in the Western Hemisphere (282 feet below sea level)?",
                 AnswerText = "California"
             });
             q.Add(new Question()
@@ -105,29 +113,36 @@ namespace BrainBuffet.Controllers
                 Id = ++index,
                 QuestionType = "text",
                 QuestionText = "What number is used most frequently in the Bible?",
-                AnswerText = "7"
+                AnswerText = "One"
             });
-            //q.Add(new Question()
-            //{
-            //    Id = ++index,
-            //    QuestionType = "text",
-            //    QuestionText = "What is  ?",
-            //    AnswerText = ""
-            //});
-            //q.Add(new Question()
-            //{
-            //    Id = ++index,
-            //    QuestionType = "text",
-            //    QuestionText = "What is  ?",
-            //    AnswerText = ""
-            //});
-            //q.Add(new Question()
-            //{
-            //    Id = ++index,
-            //    QuestionType = "text",
-            //    QuestionText = "What is  ?",
-            //    AnswerText = ""
-            //});
+            q.Add(new Question()
+            {
+                Id = ++index,
+                QuestionType = "text",
+                QuestionText = "How did the rich, young man go away after talking with the Savior?",
+                AnswerText = "Sorrowful"
+            });
+            q.Add(new Question()
+            {
+                Id = ++index,
+                QuestionType = "text",
+                ImageUrl = "https://www.mormonnewsroom.org/media/480x640/book-of-mormon.jpg",
+                QuestionText = "How many pages are there in the Book of Mormon?",
+                AnswerText = "531"
+            });
+            q.Add(new Question()
+            {
+                Id = ++index,
+                QuestionType = "text",
+                ImageUrl = "https://askgramps.org/files/2015/02/doctrine-and-covenants-open.jpg",
+                QuestionText = "How many of the 138 sections in the D&C are not from Joseph Smith (revelations, visions, prayers, instructions)?",
+                AnswerText = "5\r\n" +
+                "D&C 102 - Church council minutes\r\n" +
+                "D&C 134 - Declaration on Government\r\n" +
+                "D&C 135 - Annoucement of the Martyrdom\r\n" +
+                "D&C 136 - Revelation through Brigham Young\r\n" +
+                "D&C 138 - Vision to Joseph F. Smith"
+            });
             _questions = q;
         }
     }
